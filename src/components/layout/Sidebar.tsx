d6b3d446becf8b1495ceb8c6f306b383e4e3fe7a
Will Harris <will.t.harris@protonmail.com>
2020-05-15 10:04:06 -0700

    style sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -8,35 +8,35 @@ import {
 } from "react-icons/fa";
 
 export const Sidebar = () => (
-	<div>
-		<ul>
-			<li>
+	<div className="fixed pt-16 w-64 h-screen select-none overflow-hidden border-r border-gray-200 bg-gray-100 lg:hidden">
+		<ul className="flex flex-col p-2 pr-0 list-none text-base text-gray-800 cursor-pointer">
+			<li className="flex flex-row pl-3 pr-4 pb-3 leading-6">
 				<span>
-					<FaInbox />
+					<FaInbox className="mr-4 w-5 h-5 text-gray-700" />
 				</span>
 				<span>Inbox</span>
 			</li>
-			<li>
+			<li className="flex flex-row pl-3 pr-4 pb-3 leading-6">
 				<span>
-					<FaRegCalendar />
+					<FaRegCalendar className="mr-4 w-5 h-5 text-gray-700" />
 				</span>
 				<span>Today</span>
 			</li>
-			<li>
+			<li className="flex flex-row pl-3 pr-4 pb-3 leading-6">
 				<span>
-					<FaRegCalendarAlt />
+					<FaRegCalendarAlt className="mr-4 w-5 h-5 text-gray-700" />
 				</span>
 				<span>Next 7 Days</span>
 			</li>
 		</ul>
-
-		<div>
-			<span>
+		{/* "Middle" */}
+		<div className="flex my-3 pb-4 pl-3 cursor-pointer border-b border-gray-200">
+			<span className="self-center mr-3 text-black">
 				<FaChevronDown />
 			</span>
-			<h2>Projects</h2>
+			<h2 className="m-0 text-gray-800 text-base font-medium">Projects</h2>
 		</div>
 
-		<ul>Projects will be here</ul>
+		<ul className="pl-1">Projects will be here</ul>
 	</div>
 );
