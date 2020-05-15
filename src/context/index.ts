b00403386f9e7e291cc50c91e215b7a647590809
Will Harris <will.t.harris@protonmail.com>
2020-05-15 10:22:22 -0700

    add selected-project-context

--- src/context/index.ts
+++ src/context/index.ts
@@ -3,5 +3,17 @@ import {
 	ProjectsProvider,
 	useProjectsValue,
 } from "./projects-context";
+import {
+	SelectedProjectContext,
+	SelectedProjectProvider,
+	useSelectedProjectValue,
+} from "./selected-project-context";
 
-export { ProjectsContext, ProjectsProvider, useProjectsValue };
+export {
+	ProjectsContext,
+	ProjectsProvider,
+	useProjectsValue,
+	SelectedProjectContext,
+	SelectedProjectProvider,
+	useSelectedProjectValue,
+};
