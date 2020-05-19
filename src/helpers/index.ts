13ed87a8498b25cb1fb1342fd28b0ec7093ba202
Will Harris <will.t.harris@protonmail.com>
2020-05-19 16:21:40 -0700

    rip it all out

--- src/helpers/index.ts
+++
@@ -1,8 +0,0 @@
-import { collatedTasks } from "../constants";
-
-export const collatedTasksExist = (
-	selectedProject: string
-): { key: string; name: string } | undefined => {
-	const task = collatedTasks.find((task) => task.key === selectedProject);
-	return task;
-};
