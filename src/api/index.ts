a3e36361f2004752291379979e04324471e297e5
Will Harris <will.t.harris@protonmail.com>
2020-05-19 18:04:49 -0700

    feat: import & export queries from index file

---
+++ src/api/index.ts
@@ -0,0 +1,6 @@
+import createTodo from "./createTodo";
+import getAllTodos from "./getAllTodos";
+import editTodo from "./editTodo";
+import deleteTodo from "./deleteTodo";
+
+export { createTodo, getAllTodos, editTodo, deleteTodo };
