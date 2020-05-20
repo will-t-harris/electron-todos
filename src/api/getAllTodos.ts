eaf6dc6dabb3b005a44fc1b73041594039623849
Will Harris <will.t.harris@protonmail.com>
2020-05-19 17:56:52 -0700

    feat: add getAllTodos query

---
+++ src/api/getAllTodos.ts
@@ -0,0 +1,16 @@
+import { client, q } from "../config/db";
+
+const getAllTodos = client
+	.query(q.Paginate(q.Match(q.Ref("indexes/all_todos"))))
+	.then((response: any) => {
+		const todosRefs = response.data;
+
+		const getAllTodoDataQuery = todosRefs.map((ref: any) => {
+			return q.Get(ref);
+		});
+
+		return client.query(getAllTodoDataQuery).then((data: any) => data);
+	})
+	.catch((error: Error) => console.warn(`Error: ${error}`));
+
+export default getAllTodos;
